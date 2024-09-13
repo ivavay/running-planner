cc1edeb55b98b232c3ea534e91af26f6a13f8733
Ivy Chen <ivy0925chen@gmail.com>
2024-09-13 16:54:00 +0800

    added: basic ui for setting race info and program length

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,3 +1,34 @@
+import styled from "styled-components";
+
 export default function Home() {
-  return <h1>Home Page</h1>;
+  return (
+    <>
+      <h1>Home Page: Current program</h1>
+      <RaceInfo>
+        <RaceLabel>Race </RaceLabel>
+        <RaceTitle placeholder="e.g. Taiepi Half Marathon" />
+        <RaceDate type="date" />
+        <RaceGoal placeholder="e.g. 2:30" />
+        <SetButton>Set</SetButton>
+      </RaceInfo>
+      <ProgramInfo>
+        <ProgramLengthLabel>Program length </ProgramLengthLabel>
+        <ProgramStart type="date"></ProgramStart>
+        <ProgramEnd type="date"></ProgramEnd>
+        <SetButton>Set</SetButton>
+      </ProgramInfo>
+    </>
+  );
 }
+
+const RaceInfo = styled.div``;
+
+const RaceDate = styled.input``;
+const RaceGoal = styled.input``;
+const SetButton = styled.button``;
+const RaceLabel = styled.label``;
+const RaceTitle = styled.input``;
+const ProgramInfo = styled.div``;
+const ProgramStart = styled.input``;
+const ProgramEnd = styled.input``;
+const ProgramLengthLabel = styled.label``;
